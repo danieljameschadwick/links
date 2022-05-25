f4e00e77b20bbe26f3ba05a05cc27933e496b513
Daniel Chadwick <djchadwick@outlook.com>
2022-05-25 18:52:53 +0100

    chore: api (fixing header on frontfacing)

--- services/web/src/components/layout/Header.tsx
+++ services/web/src/components/layout/Header.tsx
@@ -3,11 +3,22 @@ import { StyleSheet, View, Text } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    position: "fixed", // investigate fixed typehint, does work
+    zIndex: 2, // add overlay constants to layout
     display: "flex",
     flexDirection: "row",
     width: "100%",
-    height: "50px",
-    padding: "12px",
+    height: 50,
+    padding: 12,
+    backgroundColor: "rgb(255,255,255)",
+    border: "1px solid rgb(215 220 224)",
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+  },
+  fixedCoverContainer: {
+    height: 50,
+    marginBottom: 25,
   },
   logoText: {
     alignSelf: "center",
@@ -23,34 +34,38 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   link: {
-    padding: "6px",
+    padding: 6,
   }
 });
 
 export const Header = () => {
   return (
-    <View style={styles.container}>
-      <ExternalLink style={styles.logoText} url={"/"} openInANewTab={false}>
-        links
-      </ExternalLink>
+    <>
+      <View style={styles.container}>
+        <ExternalLink style={styles.logoText} url={"/"} openInANewTab={false}>
+          links
+        </ExternalLink>
 
-      <View style={styles.linksContainer}>
-        <Text
-          accessibilityRole="link"
-          style={styles.link}
-          onPress={() => window.open("/login", "_self")}
-        >
-          Login
-        </Text>
+        <View style={styles.linksContainer}>
+          <Text
+            accessibilityRole="link"
+            style={styles.link}
+            onPress={() => window.open("/login", "_self")}
+          >
+            Login
+          </Text>
 
-        <Text
-          accessibilityRole="link"
-          style={styles.link}
-          onPress={() => window.open("/register", "_self")}
-        >
-          Register
-        </Text>
+          <Text
+            accessibilityRole="link"
+            style={styles.link}
+            onPress={() => window.open("/register", "_self")}
+          >
+            Register
+          </Text>
+        </View>
       </View>
-    </View>
+
+      <View style={styles.fixedCoverContainer} />
+      </>
   );
 };
